b38e2b4eefb7f934003b6d4889dd7e850ec403d0
jojo <wang.qi@ucsmy.com>
2017-04-13 17:56:34 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <Headers></Headers>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import Headers from './components/Header/Header'
+
+export default {
+  name: 'app',
+  components: {
+    Headers,
+  }
+}
+</script>
+<style lang="less" scoped>
+  @import "./style/commom.less";
+  @import "./style/sprite.less";
+  #app{
+    width: 1020px;
+    height: 670px;
+    margin: 50px auto;
+    padding: 0;
+    box-shadow: 0 0 10px 7px #e1e1e2;
+  }
+</style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="cm-header">
+
+  </div>
+</template>
+
+<script>
+  export default{
+
+  }
+</script>
+
+<style lang="less" scoped>
+  .cm-header{
+    background: #c62e3e;
+    height: 50px;
+  }
+</style>
